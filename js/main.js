a5221b160f26e0c3c857f304ff682a24f4090153
Alexander Sviridov <sviridovalexander81@gmail.com>
2021-03-22 20:59:00 +0300

    add html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('#app').vide({
+    mp4: 'video/video.mp4',
+    webm: 'video/video.webn',
+    ogv: 'video/video.ogv',
+    poster: 'img/cover-image.jpg',
+  });
+});
